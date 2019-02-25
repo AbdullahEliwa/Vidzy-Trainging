336668210bded7b0e84597770dce13c9bd294a44
Abdallah Eliwa <abdalla.eliwa2@hotmail.com>
2019-02-25 03:50:12 +0200

    Basic Customers/Movies MVC

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        List<Customer> customers = new List<Customer>
+        {
+            new Customer {Id = 1, Name = "Ahmed ALi" },
+            new Customer {Id = 2, Name = "Mohamed ALi" }
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            Customer customer = customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        IEnumerable<Movie> Movies = new List<Movie>
+        {
+            new Movie { Id = 1, Name = "Harry Potter"}
+        };
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(Movies);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var movie = Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
+    }
+}
