ea11c8e680c42e621cff3be702bb26aacd03dedf
Abdallah Eliwa <abdalla.eliwa2@hotmail.com>
2019-03-02 11:58:50 +0200

    Build APIs for CRUD operation for customers

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        
+        public int MembershipTypeId { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        
+        public DateTime? Birthdate { get; set; }
+    }
+}
