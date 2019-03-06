59ceb4d6b5859a92cd854ae6417d46021a4a9906
Abdallah Eliwa <abdalla.eliwa2@hotmail.com>
2019-03-06 13:57:54 +0200

    Prevent anonymous access globally.

--- Vidly/App_Start/FilterConfig.cs
+++ Vidly/App_Start/FilterConfig.cs
@@ -7,6 +7,7 @@ namespace Vidly
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new AuthorizeAttribute());
             filters.Add(new HandleErrorAttribute());
         }
     }
